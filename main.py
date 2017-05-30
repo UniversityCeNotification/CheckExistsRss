59fe279e3bddf38f00b37ae8a30255bc75e9bc6a
pleycpl <h4rvey@protonmail.com>
2017-05-30 13:11:33 +0000

    checking timeout

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import json
 import re
 import feedparser
+import io
 from lxml import html
 from urllib.parse import urljoin
 
@@ -34,11 +35,13 @@ def feedfinder(url, domain):
     feedUrls = tree.xpath("//link[@rel='alternate']")
     if feedUrls:
         for feed in feedUrls:
-            t = feed.xpath('@type')[0]
+            t = feed.xpath('@type')
             if t:
+                t = t[0]
                 if "rss" in t or "xml" in t:
-                    href = feed.xpath('@href')[0]
+                    href = feed.xpath('@href')
                     if href:
+                        href = href[0]
                         possibleFeeds.append(urljoin(url, href))
 
     atags = tree.xpath("//a")
@@ -47,11 +50,19 @@ def feedfinder(url, domain):
         if href:
             href = href[0]
             if "xml" in href or "rss" in href or "feed" in href:
-                possibleFeeds.append(urljoin(domain, href))
+                possibleFeeds.append(urljoin(url, href))
 
-    print(possibleFeeds)
     for link in list(set(possibleFeeds)):
-        f = feedparser.parse(link)
+        # Thanks for https://stackoverflow.com/questions/9772691/feedparser-with-timeout
+        try:
+            resp = requests.get(link, timeout=10.0)
+        except Exception as e:
+            print("Timeout when reading RSS %s", link, ' e:', e)
+            return
+
+        content = io.BytesIO(resp.content)
+
+        f = feedparser.parse(content)
         if len(f.entries) > 0:
             if url not in result:
                 result.append(link)
@@ -65,8 +76,8 @@ def main():
             result = feedfinder(site['web_page'], site['domain'])
             print(result)
             if result:
-                print('Rss Exists: ', rssLink)
-                dumpJson(site['name'], site['domain'], rssLink)
+                print('Rss Exists: ', result[0])
+                dumpJson(site['name'], site['domain'], result[0])
 
 if __name__ == '__main__':
     main()
